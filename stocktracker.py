f5575123deeb4d695bdc0f8ed889d58400bf2b6f
jcwyatt <jwyatt043@googlemail.com>
2016-07-04 14:20:04 +0100

    Update stocktracker.py
    getting the data is now a function.

--- stocktracker.py
+++ stocktracker.py
@@ -33,34 +33,39 @@ def email(msg): #Send an email with the string payload 'msg'
 	server.quit()
 
 
+def getStockData():
+	#get the data from yahoo stock api (hsbc, vw, ftse, euro) and convert to a string
+	#n = name |g = day's low | h = day's high | b = bid | p2 = day's change %
 
-#get the data from yahoo stock api (hsbc, vw, ftse, euro) and convert to a string
-#n = name |g = day's low | h = day's high | b = bid | p2 = day's change %
+	r = str(urlopen('http://finance.yahoo.com/d/quotes.csv?s=HSBA.L+VOW.DE+^FTSE+EURGBP=X&f=nghbp2').read())
 
-r = str(urlopen('http://finance.yahoo.com/d/quotes.csv?s=HSBA.L+VOW.DE+^FTSE+EURGBP=X&f=nghbp2').read())
+	#print ('Response from URLLIB: \n',r)
+	#print('\nEnd of URLIB response')
 
-#print ('Response from URLLIB: \n',r)
-#print('\nEnd of URLIB response')
+	#parse the data into the list stockData
 
-#parse the data into the list stockData
+	dataSet=[]
+	datum=''
 
-stockData=[]
-datum=''
+	for i in r:
+		datum = datum+i
+		if i=="\"" or i==",":
+			dataSet.append(datum[:-1])
+			datum=''
+	return (dataSet)
 
-for i in r:
-	datum = datum+i
-	if i=="\"" or i==",":
-		stockData.append(datum[:-1])
-		datum=''
+
+stockData = getStockData()
+
 
 #print everything in the list
 print ('stockData = ',stockData)
 
 #contruct the payload
 message = stockData[1] + stockData[5] + "\n" + \
-          stockData[9] + stockData[13] + "\n" + \
-          stockData[17] + stockData[] + "\n" + \
-          stockData[25] + stockData[29]
+	  stockData[9] + stockData[13] + "\n" + \
+	  stockData[17] + stockData[19] + "\n" + \
+	  stockData[25] + stockData[29]
 
 print("Payload :\n", message)
 
